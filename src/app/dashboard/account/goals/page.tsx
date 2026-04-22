34ea224670938f9e01a9a7507c60591d9148ebb9
Rin <82919982+Rin-chan@users.noreply.github.com>
2026-04-22 23:11:34 +0800

    Added streak frontend

--- src/app/dashboard/account/goals/page.tsx
+++ src/app/dashboard/account/goals/page.tsx
@@ -10,13 +10,50 @@ import CardContent from "@mui/material/CardContent";
 import Divider from '@mui/material/Divider/Divider';
 import Button from '@mui/material/Button/Button';
 import { AccountGoalForm } from '@/components/dashboard/account/account-goal-form';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
+import { isWithinInterval } from "date-fns";
 
 export default function Page(): React.JSX.Element {
     const [showUpdateGoalsForm, setShowUpdateGoalsForm] = React.useState(false);
     
+    const streak = 1;
+    const longestStreak = 2;
+
     const toggleUpdateGoalsForm = (): void => {
         setShowUpdateGoalsForm(!showUpdateGoalsForm);
     };
+
+    const [dates, setDates] = useState([new Date(2026, 3, 1), new Date(2026, 3, 2), new Date(2026, 3, 20)]);
+
+    const dateAlreadyClicked = (dates, date) => dates.some(
+        d => new Date(d).getTime() === new Date(date).getTime()
+    )
+
+    const disabledRanges = [[new Date(2020, 1, 1), new Date()]];
+
+    const tileClassName = ({ date }) => {
+        if (dateAlreadyClicked(dates, date)) {
+            return 'react-calendar__tile--active'
+            /* this is react-calendar's default class name for an active
+            tile, but you can use any custom class name of your choice */
+        }
+    }
+
+    function isWithinRange(date, range) {
+        return isWithinInterval(date, { start: range[0], end: range[1] });
+    }
+
+    function isWithinRanges(date, ranges) {
+        return ranges.some(range => isWithinRange(date, range)) && !dateAlreadyClicked(dates, date);
+    }
+
+    function tileDisabled({ date, view }) {
+        if (view === 'month') {
+            return isWithinRanges(date, disabledRanges);
+        }
+    }
 
     return (
     <Stack spacing={3}>
@@ -55,7 +92,19 @@ export default function Page(): React.JSX.Element {
             />
 
             <CardContent sx={{pb: '16px'}}>
-                
+                <Stack direction="row" justifyContent="space-around">
+                    <Grid container spacing={3}>
+                        <Grid xs={12}>
+                            <Typography variant="body1">Current Streak: {streak}</Typography>
+                            <Typography variant="body1">Longest streak: {longestStreak}</Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Calendar
+                        tileClassName={tileClassName}
+                        tileDisabled={tileDisabled}
+                    />
+                </Stack>
             </CardContent>
         </Card>
 
